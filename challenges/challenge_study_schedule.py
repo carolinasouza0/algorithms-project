8428736b0bda40b8728bcc180a2ef903c1822ec8
Carolina Pedrosa <carolinasouza0@gmail.com>
2024-01-25 13:02:11 -0300

    feat: implement challenge study schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,19 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if target_time is None:
+        return None
+
+    students_online = 0
+
+    for temp_in, temp_out in permanence_period:
+        if (
+            temp_in is None
+            or temp_out is None
+            or not isinstance(temp_in, int)
+            or not isinstance(temp_out, int)
+        ):
+            return None
+
+        if isinstance(target_time, int) and temp_in <= target_time <= temp_out:
+            students_online += 1
+
+    return students_online
